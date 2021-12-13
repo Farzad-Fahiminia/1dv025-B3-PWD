6a3ccc6e28911e14416d50663e8d156d183b7bd1
Farzad Fahiminia <ff222cb@student.lnu.se>
2021-12-13 09:54:19 +0100

    Add desktop component

---
+++ src/components/desktop/desktop.js
@@ -0,0 +1,6 @@
+/**
+ * The desktop script file of the application.
+ *
+ * @author Farzad Fahiminia <ff222cb@student.lnu.se>
+ * @version 1.0.0
+ */
---
+++ src/components/desktop/index.js
@@ -0,0 +1,9 @@
+/**
+ * The countdown timer script file of the application.
+ *
+ * @author Farzad Fahiminia <ff222cb@student.lnu.se>
+ * @version 1.0.0
+ */
+
+// It is best practice to have a index.js as root of the component. You can add the content in desktop.js in this file if you like, or import as this.
+import './desktop.js'
