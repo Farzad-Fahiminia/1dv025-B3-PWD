31537dcbac450836e00bc5df604ed3ff73ca912f
Farzad Fahiminia <ff222cb@student.lnu.se>
2021-12-16 15:07:58 +0100

    Add open window to memory placeholder

--- src/js/components/my-desktop/my-desktop.js
+++ src/js/components/my-desktop/my-desktop.js
@@ -92,88 +92,21 @@ customElements.define('my-desktop',
       this.attachShadow({ mode: 'open' })
         .appendChild(template.content.cloneNode(true))
 
-      // Bind event handlers of child elements.
-      this._onSubmit = this._onSubmit.bind(this)
-      this._formElement.addEventListener('submit', this._onSubmit)
+      this.myDesktop = this.shadowRoot.querySelector('.my-desktop')
+
+      this.myMemoryApp = this.shadowRoot.querySelector('#memory-app')
+      this.myMemoryApp.addEventListener('click', (event) => {
+        console.log('KLICK PÅ MEMORY APP')
+        this.openAppWindow(event)
+      })
     }
 
-    /**
-    * Attributes to monitor for changes.
-    *
-    * @returns {string[]} A string array of attributes to monitor.
-    */
-    static get observedAttributes () {
-      return ['message']
-    }
-
-    /**
-    * Called after the element is inserted into the DOM.
-    */
-    connectedCallback () {
-      if (!this.hasAttribute('message')) {
-        this.setAttribute('message', 'Prepare for the quiz"')
-      }
-      this._upgradeProperty('message')
-    }
-
-    /**
-    * Called when observed attribute(s) changes.
-    *
-    * @param {string} name - The attribute's name.
-    * @param {*} oldValue - The old value.
-    * @param {*} newValue - The new value.
-    */
-    attributeChangedCallback (name, oldValue, newValue) {
-      if (name === 'message') {
-        this._messageBoard.textContent = newValue
-      }
-    }
-
-    /**
-    * Called after the element has been removed from the DOM.
-    */
-    disconnectedCallback () {
-    }
-
-    /**
-    * Run the specified instance property
-    * through the class setter.
-    *
-    * @param {string} prop - The property's name.
-    */
-    _upgradeProperty (prop) {
-      if (Object.hasOwnProperty.call(this, prop)) {
-        const value = this[prop]
-        delete this[prop]
-        this[prop] = value
-      }
-    }
-
-    /**
-    * Gets the message.
-    *
-    * @returns {string} The message value.
-    */
-    get message () {
-      return this.getAttribute('message')
-    }
-
-    /**
-    * Sets the message.
-    *
-    * @param {string} value - The message.
-    */
-    set message (value) {
-      if (this.message !== value) {
-        this.setAttribute('message', value)
-      }
-    }
-
-    /**
-    * Cleans the message board.
-    */
-    clean () {
-      this._messageBoard.textContent = ''
+    openAppWindow () {
+      console.log('NU ÄR DU INNE I OPEN APP!')
+      const appWindow = document.createElement('my-app-window')
+      console.log(appWindow)
+      console.log(this.myDesktop)
+      this.myDesktop.append(appWindow)
     }
   }
 )
